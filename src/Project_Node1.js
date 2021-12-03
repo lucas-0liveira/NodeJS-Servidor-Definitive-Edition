375027bb8daf7a2e7f77bf365d9aa800613b8560
Lucas de Oliveira Fernandes <lucas.oliver.70@hotmail.com>
2021-12-03 13:06:30 -0300

    Project Node Part 1

--- src/Project_Node1.js
+++ src/Project_Node1.js
@@ -1,4 +1,4 @@
 function enviaremail (nome, email) {
-    console.log ("Encaminhar email para" + email + "que é destinado ao" + nome);
+    console.log ("Encaminhar email para" + nome + "que é destinado ao" + email);
 }
 enviaremail (" lucas ", " lucas.oliver.70@hotmail.com ");
