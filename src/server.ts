404387e2690c17c882a45cc2b9b49fe78f2c2680
Lucas de Oliveira Fernandes <lucas.oliver.70@hotmail.com>
2021-12-03 12:50:01 -0300

    server in typescript atualizado

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,11 @@ const app = express();
 app.get("/test", (request, response) => {
     //Request => Tudo que está entrando
     //Response => Tudo que está saindo
-    return response.send("Olá Usuário");
+    return response.send("Olá Usuário, esse é o método GET");
 });
 
 app.post("/test-post", (request, response) => {
- return response.send("Olá Usário, esse é o método POST")
+ return response.send("Olá Usuário, esse é o método POST")
 });
 
 // http://localhost:3000
