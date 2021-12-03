106dc2abb314f6dd05c0c506a17b469f446cf588
Lucas de Oliveira Fernandes <lucas.oliver.70@hotmail.com>
2021-12-03 12:21:36 -0300

    NodeJS

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+// @types/express
+const app = express();
+
+/**
+ * GET => Buscar Informação
+ * POST => Inserir/Criar uma informação 
+ * PUT => Alterar uma Informação
+ * DELETE => Remover um dado
+ * PATCH => Alterar uma informação específica
+ */
+
+app.get("/test", (request, response) => {
+    //Request => Tudo que está entrando
+    //Response => Tudo que está saindo
+    return response.send("Olá NLW");
+});
+
+app.post("/test-post", (request, response) => {
+ return response.send("Olá NLW método POST")
+});
+
+// http://localhost:3000
+app.listen(3000, () => console.log("server is running"));
