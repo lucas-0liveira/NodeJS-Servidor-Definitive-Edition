4aad497395ded394592d4f263cef8ff599fceb98
lucas-0liveira <lucas.oliver.70@hotmail.com>
2021-12-10 11:47:19 -0300

    Project - server version 2

--- src/Project_Node1.js
+++
@@ -1,4 +0,0 @@
-function enviaremail (nome, email) {
-    console.log ("Encaminhar email para" + email + "que é destinado ao" + nome);
-}
-enviaremail (" lucas ", " lucas.oliver.70@hotmail.com ");
--- src/index.js
+++
@@ -1,1 +0,0 @@
-console.log("Iniciando o novo projeto NodeJS")
