3bba4c101d17ee45c3dfd27e27db04dcf0081bd2
lucas-0liveira <lucas.oliver.70@hotmail.com>
2021-12-26 17:23:37 -0300

    Project Server Version 2

--- src/server_V2.ts
+++ src/server_V2.ts
@@ -1,9 +1,15 @@
 import "reflect-metadata";
 import express from "express";
 
+import {router} from "./routes";
+
 import "./database";
 
-const app = express ();
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 // http://localhost:3000
 app.listen(3000, () => console.log("server user running versão 2"));
