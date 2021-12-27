432e1fb50322a3553777d3a513f15e43876f9798
lucas-0liveira <lucas.oliver.70@hotmail.com>
2021-12-27 10:24:15 -0300

    Project - Server Version 2

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,33 +1,6 @@
-import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
-import { v4 as uuid } from "uuid";
-@Entity("users")
-class User{
+import {Entity} from "typeorm";
 
-    @PrimaryColumn()
-    readonly id: string;
+@Entity()
+export class User {
 
-    @Column()
-    name: string;
-
-    @Column()
-    email: string; 
-
-    @Column()
-    admin: boolean;
-
-    @CreateDateColumn()
-    created_at: Date;
-
-    @UpdateDateColumn()
-    updated_at: Date;
-
-    constructor(){
-        if(!this.id){
-            this.id = uuid();
-        }
-    }
 }
-
-export {User};
-
-//entidade < - > ORM < - > BD (users)
--- src/server_V2.ts
+++ src/server_V2.ts
@@ -1,15 +1,9 @@
 import "reflect-metadata";
 import express from "express";
 
-import {router} from "./routes";
-
 import "./database";
 
 const app = express();
 
-app.use(express.json());
-
-app.use(router);
-
 // http://localhost:3000
-app.listen(3000, () => console.log("server user running versão 2"));
+app.listen(3000, () => console.log(" server user running V2"));
