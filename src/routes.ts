ba4bcfa074c7df4491d41b6b3d07da2b3e7939d2
lucas-0liveira <lucas.oliver.70@hotmail.com>
2021-12-27 11:50:50 -0300

    Project - Server Version 2

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import {request, response, Router} from "express";
+import { CreateUserController } from "./controllers/CreateUserController";
+
+const router = Router();
+
+const createUserController = new CreateUserController();   
+
+router.post("/users", createUserController.handle);
+
+export {router}
--- src/server_V2.ts
+++ src/server_V2.ts
@@ -1,9 +1,13 @@
 import "reflect-metadata";
 import express from "express";
 
+import {router} from "./routes";
+
 import "./database";
 
 const app = express();
+
+app.use(router);
 
 // http://localhost:3000
 app.listen(3000, () => console.log(" server user running V2"));
