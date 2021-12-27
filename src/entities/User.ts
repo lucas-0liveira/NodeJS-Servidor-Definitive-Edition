454bfd53b0eed316c85f228374a71c355d771818
lucas-0liveira <lucas.oliver.70@hotmail.com>
2021-12-27 11:50:32 -0300

    Project Server Version 2

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,8 +1,37 @@
-import {Entity} from "typeorm";
+import {Entity, 
+PrimaryColumn,
+Column,
+CreateDateColumn,
+UpdateDateColumn,
+UpdateQueryBuilder} from "typeorm";
+import {v4 as uuid} from "uuid";
 
 @Entity("users")
 class User {
 
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at:Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor(){
+        if(!this.id){
+            this.id = uuid();
+        }
+    }
 }
 
 export {User};
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,39 @@
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+
+class CreateUserService {
+
+    async execute ({ name, email, admin}: IUserRequest){
+        const usersRepository = new UsersRepositories();
+
+        if(!email){
+            throw new Error ("Email incorrect");    
+        }
+
+        const userArealdyExists = await usersRepository.findOne({
+            email,
+        });
+        
+        if (userArealdyExists){
+            throw new Error ("User already exists");
+        }
+        
+        const user = usersRepository.create ({
+            name,
+            email,
+            admin
+        })
+
+        await usersRepository.save (user);
+
+        return user;
+    }
+
+}
+
+export {CreateUserService}
